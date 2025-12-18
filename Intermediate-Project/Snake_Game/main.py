1188b2a36b05ccb4b78cc7081ac2aacba533abb7
Voshana08 <134561940+Voshana08@users.noreply.github.com>
2025-12-18 13:26:36 +1100

    keyboard directional input

--- Intermediate-Project/Snake_Game/main.py
+++ Intermediate-Project/Snake_Game/main.py
@@ -12,9 +12,12 @@ screen.title("Snake game")
 screen.tracer(0)
 #creating a snake object from the snake class 
 snake = Snake()
-
-
-    
+# Listneing to key strokes
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
 
 
 game_is_on = True

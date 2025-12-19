ccad3170aea46fc359811401cf93c8ffe123361b
Voshana08 <134561940+Voshana08@users.noreply.github.com>
2025-12-19 19:23:59 +1100

    Turtle crossing final

--- Intermediate-Project/turtle-crossing-start/car_manager.py
+++ Intermediate-Project/turtle-crossing-start/car_manager.py
@@ -1,7 +1,35 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+# CarManager does not inherit from Turtle because it is not a drawable object.
+# Instead, it manages multiple Turtle car objects using composition.
+# Inheritance is only needed when a class is a specialized version of another.
+# Here, CarManager "has cars" rather than "is a car," so no superclass is required.
 
-class CarManager:
-    pass
+class CarManager():
+    def __init__(self):
+        
+        self.all_cars =[]
+        self.car_speed =STARTING_MOVE_DISTANCE
+    def create_car(self):
+        random_chance = random.randint(1,6)
+        #this ensure very six times the car is generated
+        if random_chance ==1 :
+            new_car = Turtle('square')
+            new_car.shapesize(stretch_wid=1,stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250,250)
+            new_car.goto(300,random_y)
+            self.all_cars
+            self.all_cars.append(new_car)
+        
+    def move_cars(self):
+        for car in self.all_cars:
+           car.backward(STARTING_MOVE_DISTANCE)
+   
+    def level_up(self):
+        self.car_speed+=MOVE_INCREMENT     
--- Intermediate-Project/turtle-crossing-start/main.py
+++ Intermediate-Project/turtle-crossing-start/main.py
@@ -1,11 +1,19 @@
 import time
-from turtle import Screen
+from turtle import Screen,Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+
+player =Player()
+carmanager = CarManager()
+scoreboard=Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up,"Up")
+# controls how often the screen refreshes; lower values = faster animation
 screen.tracer(0)
 
 game_is_on = True
@@ -14,3 +22,21 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    carmanager.create_car()
+    carmanager.move_cars()
+    #Detecting collision with car and the player
+    for car in carmanager.all_cars:
+        if car.distance(player) <20:
+            game_is_on =False
+    
+    #Detect a succesful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        carmanager.level_up()
+        scoreboard.update_scoreboard()
+        scoreboard.increase_level()
+
+scoreboard.game_over()
+        
+screen.exitonclick()
+    
